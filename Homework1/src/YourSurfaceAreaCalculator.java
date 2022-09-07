c00c23f906f0a0161a8e9e99065220fc3f6c47ff
Taylor Jensen <Taylorlcj@gmail.com>
2022-09-07 12:42:57 -0400

    Homework 1

--- Homework1/src/YourSurfaceAreaCalculator.java
+++ Homework1/src/YourSurfaceAreaCalculator.java
@@ -1,3 +1,11 @@
+//**********************************************************
+/* Class: YourSurfaceAreaCalculator
+Author: Taylor Jensen
+Date Modified: Sep 7, 2022
+Purpose: First class edit-compile-run demonstration
+Attributes: none
+Methods: +main(): void */
+//************************************************************
 public class YourSurfaceAreaCalculator {
     public static void main(String[] args) {
 
