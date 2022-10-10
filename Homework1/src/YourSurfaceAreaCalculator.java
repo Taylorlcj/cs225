8b78f0eb77f3d69ddc1389e8099a8922bc472c09
Taylor Jensen <Taylorlcj@gmail.com>
2022-10-10 16:40:42 -0400

    Homework 1

--- Homework1/src/YourSurfaceAreaCalculator.java
+++ Homework1/src/YourSurfaceAreaCalculator.java
@@ -7,6 +7,7 @@
 ********************************************************
 */
 
+//I apologize if this sucks, I will do better next time
 import java.util.Scanner;
 public class YourSurfaceAreaCalculator {
     public static void main( String[] args ) {
@@ -17,7 +18,7 @@ public class YourSurfaceAreaCalculator {
         length = 0.0;
         height = 0.0;
         width = 0.0;
-        System.out.println("Test case 1: length = height = width = 0" );
+        System.out.println("First Test case: length = height = width = 0" );
         System.out.println("Expected results: SA = 0.0, SASI = 0.0");
         System.out.print("Surface area in inches squared, SA = " );
         System.out.println(2 * (length*height + width*height + length*width) + ".");
@@ -28,7 +29,7 @@ public class YourSurfaceAreaCalculator {
         length = 15.0;
         height = 10.0;
         width = 0.0;
-        System.out.println("Test case 2: length = 15.0, height = 10.0, width = 0.0" );
+        System.out.println("Second Test case: length = 15.0, height = 10.0, width = 0.0" );
         System.out.println("Expected results: SA = 300.0, SASI = 1935.48");
         System.out.print("Surface area in square inches, SA = " );
         System.out.println(2 * (length*height + width*height + length*width) + ".");
@@ -39,7 +40,7 @@ public class YourSurfaceAreaCalculator {
         length = 10.0;
         height = 1.0;
         width = 1.0;
-        System.out.println("Test case 3: length = 10.0, height = 1.0, width = 1.0" );
+        System.out.println("Third Test case: length = 10.0, height = 1.0, width = 1.0" );
         System.out.println("Expected results: SA = 42.0, SASI = 270.97");
         System.out.print("Surface area in square inches, SA = " );
         System.out.println(2 * (length*height + width*height + length*width) + ".");
@@ -49,7 +50,7 @@ public class YourSurfaceAreaCalculator {
 
         System.out.print("Input length please: ");
         length = in.nextDouble();
-        System.out.print("Input height plese: ");
+        System.out.print("Input height please: ");
         height = in.nextDouble();
         System.out.print("Input width please: ");
         width = in.nextDouble();
