9060c09f0f5d11d4dd06e8a6f970a32e8dd28656
Taylor Jensen <Taylorlcj@gmail.com>
2022-10-10 01:36:09 -0400

    Homework 1

--- Homework1/src/YourSurfaceAreaCalculator.java
+++ Homework1/src/YourSurfaceAreaCalculator.java
@@ -12,54 +12,50 @@ public class YourSurfaceAreaCalculator {
     public static void main( String[] args ) {
         double length, height, width;
         Scanner in = new Scanner( System.in );
-// Test case 1: length = height = width = 0
+
+        // First Test case
         length = 0.0;
         height = 0.0;
         width = 0.0;
         System.out.println("Test case 1: length = height = width = 0" );
         System.out.println("Expected results: SA = 0.0, SASI = 0.0");
-        System.out.print("Surface area in square inches, SA = " );
-        System.out.println(2 * (length*height + width*height + length*width) +
-                ".");
+        System.out.print("Surface area in inches squared, SA = " );
+        System.out.println(2 * (length*height + width*height + length*width) + ".");
         System.out.print("Surface area in square centimeters, SASI = " );
-        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height +
-                length*width) + ".\n\n");
-// Test case 2: length = height = width = 0
+        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height + length*width) + ".\n\n");
+
+        // Second Test case
         length = 15.0;
         height = 10.0;
         width = 0.0;
         System.out.println("Test case 2: length = 15.0, height = 10.0, width = 0.0" );
         System.out.println("Expected results: SA = 300.0, SASI = 1935.48");
         System.out.print("Surface area in square inches, SA = " );
-        System.out.println(2 * (length*height + width*height + length*width) +
-                ".");
+        System.out.println(2 * (length*height + width*height + length*width) + ".");
         System.out.print("Surface area in square centimeters, SASI = " );
-        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height +
-                length*width) + ".\n\n");
-// Test case 3: length = height = width = 0
+        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height + length*width) + ".\n\n");
+
+        // Third Test case
         length = 10.0;
         height = 1.0;
         width = 1.0;
         System.out.println("Test case 3: length = 10.0, height = 1.0, width = 1.0" );
         System.out.println("Expected results: SA = 42.0, SASI = 270.97");
         System.out.print("Surface area in square inches, SA = " );
-        System.out.println(2 * (length*height + width*height + length*width) +
-                ".");
+        System.out.println(2 * (length*height + width*height + length*width) + ".");
         System.out.print("Surface area in square centimeters, SASI = " );
-        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height +
-                length*width) + ".\n\n");
-// Allow user to input values (no exception handling or verification of input)
-        System.out.print("Input the length: ");
+        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height + length*width) + ".\n\n");
+
+
+        System.out.print("Input length please: ");
         length = in.nextDouble();
-        System.out.print("Input the height: ");
+        System.out.print("Input height plese: ");
         height = in.nextDouble();
-        System.out.print("Input the width: ");
+        System.out.print("Input width please: ");
         width = in.nextDouble();
         System.out.print("Surface area in square inches, SA = " );
-        System.out.println(2 * (length*height + width*height + length*width) +
-                ".");
+        System.out.println(2 * (length*height + width*height + length*width) + ".");
         System.out.print("Surface area in square centimeters, SASI = " );
-        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height +
-                length*width) + ".\n\n");
+        System.out.println(2.54 * 2.54 * 2 * (length*height + width*height + length*width) + ".\n\n");
     }
 }
