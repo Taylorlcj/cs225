b79c454753df34deccb904d85cbb0f8a733baf7d
Taylor Jensen <Taylorlcj@gmail.com>
2022-10-11 18:42:08 -0400

    Homework 2 10/11/22

---
+++ Homework2/src/GameController.java
@@ -0,0 +1,14 @@
+/*
+*******************************************************
+ Class: GameController
+ Author: Taylor Jensen
+ Date Created: Oct 11, 2022
+ Attributes: None
+********************************************************
+*/
+public class GameController {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ Homework2/src/PrizeBoxes.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+public class PrizeBoxes {
+    private int[] prizes;
+
+    // ***** Methods *****
+    public void createPrizeBoxes(int numBoxes) {
+        prizes = new int[numBoxes];
+    }
+
+    public void initializePrizeBoxes() {
+        for (int i = 0; i < prizes.length; i++) {
+            prizes[i] = (int) Math.floor(81 * Math.random()) + 20;
+        }
+    }
+
+    public void initializeSpecialPrizes() {
+        int index = (int) Math.floor(prizes.length * Math.random());
+        prizes[index] = 0;
+        index = (int) Math.floor(prizes.length * Math.random());
+        prizes[index] = -100;
+    }
+}
