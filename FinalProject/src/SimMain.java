e712bad4c18a48230928d6278171ab18fcf102d3
Taylor Jensen <Taylorlcj@gmail.com>
2022-12-09 19:25:38 -0500

    Final Project 12/9/22

--- FinalProject/src/SimMain.java
+++ FinalProject/src/SimMain.java
@@ -1,3 +1,16 @@
+/*
+*******************************************************
+ Class: SimMain
+ Author: Taylor Jensen
+ Date Created: Dec 8, 2022
+ Purpose:This class allows the game to start and end
+ Attributes:
+
+ Method:
+
+
+********************************************************
+*/
 public class SimMain {
     public static void main(String[] args){
         GameController controller = new GameController();
